d336bb6486149e8e8a25b86db4c3914519ee37ec
ozudev <ozudev@gmail.com>
2024-07-03 16:06:39 +0300

    CRUD -> read ve create islemlerini yaptik

--- ASP.NET CORE MVC/Controllers/HomeController.cs
+++ ASP.NET CORE MVC/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ASP.NET_CORE_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ ASP.NET CORE MVC/Controllers/KitapTuruController.cs
@@ -0,0 +1,43 @@
+﻿using ASP.NET_CORE_MVC.Models;
+using ASP.NET_MVC.Entility;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ASP.NET_CORE_MVC.Controllers
+{
+    public class KitapTuruController : Controller
+    {
+        private readonly UygulamaDbContext _kitapTuruContext;
+
+        public KitapTuruController(UygulamaDbContext kitapTuruContext)
+        {
+            _kitapTuruContext = kitapTuruContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<KitapTuru> kitapTurleri = _kitapTuruContext.KitapTurleri.ToList();
+            return View(kitapTurleri);
+        }
+
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ekle(KitapTuru item)
+        {
+            if (ModelState.IsValid)
+            {
+                _kitapTuruContext.KitapTurleri.Add(item);
+                _kitapTuruContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
