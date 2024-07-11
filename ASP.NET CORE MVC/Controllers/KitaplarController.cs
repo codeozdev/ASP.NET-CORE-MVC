3530fecc698e71910019c5bd6449f3d7ebfe4575
ozudev <ozudev@gmail.com>
2024-07-11 18:37:26 +0300

    Kitaplar güncelleme işlemi

--- ASP.NET CORE MVC/Controllers/KitaplarController.cs
+++ ASP.NET CORE MVC/Controllers/KitaplarController.cs
@@ -56,5 +56,40 @@ namespace ASP.NET_CORE_MVC.Controllers
                 return View();
             }
         }
+
+        public IActionResult Guncelle(int id)
+        {
+            Kitaplar kitap = _kitaplarContext.Kitaplar.Find(id);
+            return View(kitap);
+        }
+
+        [HttpPost]
+        public IActionResult Guncelle(Kitaplar item, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                // Resim yükleme işlemi
+                string wwwRootPath = _webHostEnvironment.WebRootPath; // wwwroot path
+                string kitapPath = Path.Combine(wwwRootPath, @"img");
+
+                if (file != null)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(kitapPath, file.FileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    item.KitapResim = @"\img\" + file.FileName;
+                }
+
+                _kitaplarContext.Kitaplar.Update(item);
+                _kitaplarContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
